7921ae0fad6e3adc9cc4c38084ed801e9c5a0a1f
Luiz Paulo <lppjunior@gmail.com>
2020-09-03 17:00:52 -0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    minefield: './src'
+  },
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Minefield JS',
+      favicon: 'src-example/favicon.png',
+      template: 'src-example/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src-example/styles.css', to: '' },
+        { from: 'src-example/main.js', to: '' },
+        { from: 'src-example/minefield-client.js', to: '' }
+      ]
+    })
+  ],
+
+  // webpack-dev-server configurations
+  devServer: {
+    compress: true,
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+    port: 9000
+  }
+}
