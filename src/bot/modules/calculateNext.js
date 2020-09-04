ef036fe85ae3ccdff9b61d0a595156549df9bc46
Luiz Paulo <lppjunior@gmail.com>
2020-09-04 19:25:44 -0300

    * Remove log * Refactor code

--- src/bot/modules/calculateNext.js
+++ src/bot/modules/calculateNext.js
@@ -10,58 +10,52 @@ export default {
 
   addFlag: function () {
     Object
-    .keys(Minefield.CHECKERS)
-    .filter((key) => ![
+      .keys(Minefield.CHECKERS)
+      .filter((key) => ![
         Minefield.CHECKERS.EMPTY,
         Minefield.CHECKERS.FLAG
       ].includes(Minefield.CHECKERS[key])
-    ).forEach((key) => {
-      this.boardMap[Minefield.CHECKERS[key]].forEach(checker => {
-        if (checker.value === checker.around[Minefield.CHECKERS.EMPTY].length + checker.around[Minefield.CHECKERS.FLAG].length) {
-          const itens = checker.around[Minefield.CHECKERS.EMPTY]
-          itens.forEach(item => {
-            item.type = Minefield.CHECKERS.FLAG
-          })
+      ).forEach((key) => {
+        this.boardMap[Minefield.CHECKERS[key]].forEach(checker => {
+          if (checker.value === checker.around[Minefield.CHECKERS.EMPTY].length + checker.around[Minefield.CHECKERS.FLAG].length) {
+            const itens = checker.around[Minefield.CHECKERS.EMPTY]
+            itens.forEach(item => {
+              item.type = Minefield.CHECKERS.FLAG
+            })
 
-          this.nextList = [...this.nextList, ...itens]
-        }
+            this.nextList = [...this.nextList, ...itens]
+          }
+        })
       })
-    })
-
-    console.warn('ADD FLAG', this.nextList.length)
-
   },
 
   addOpen: function () {
     Object
-    .keys(Minefield.CHECKERS)
-    .filter((key) => ![
+      .keys(Minefield.CHECKERS)
+      .filter((key) => ![
         Minefield.CHECKERS.EMPTY,
         Minefield.CHECKERS.FLAG
       ].includes(Minefield.CHECKERS[key])
-    ).forEach((key) => {
-      this.boardMap[Minefield.CHECKERS[key]].forEach(checker => {
-        if (checker.value === checker.around[Minefield.CHECKERS.FLAG].length) {
-          const itens = checker.around[Minefield.CHECKERS.EMPTY]
-          itens.forEach(item => {
-            item.origin = 'ADD_OPEN'
-          })
+      ).forEach((key) => {
+        this.boardMap[Minefield.CHECKERS[key]].forEach(checker => {
+          if (checker.value === checker.around[Minefield.CHECKERS.FLAG].length) {
+            const itens = checker.around[Minefield.CHECKERS.EMPTY]
+            itens.forEach(item => {
+              item.origin = 'ADD_OPEN'
+            })
 
-          this.nextList = [...this.nextList, ...itens]
-        }
+            this.nextList = [...this.nextList, ...itens]
+          }
+        })
       })
-    })
-
-    console.warn('ADD OPEN', this.nextList.length)
   },
 
   addRand: function () {
     if (this.state.checked === 0 || this.nextList.length === 0) {
       const rand = (size) => Math.floor(Math.random() * size)
       const emptyCheckers = this.boardMap[Minefield.CHECKERS.EMPTY]
-      const checker = { origin: 'RANDOM', ...emptyCheckers[rand(emptyCheckers.length)]}
+      const checker = { origin: 'RANDOM', ...emptyCheckers[rand(emptyCheckers.length)] }
       this.nextList.push(checker)
-      console.warn('ADD RANDOMIC PLAYER', this.nextList.length)
     }
   },
 
