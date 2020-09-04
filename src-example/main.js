c0147a1e711a58832116548e8acef8875866c76c
Luiz Paulo <lppjunior@gmail.com>
2020-09-04 10:03:53 -0300

    Add bot check Flag

--- src-example/main.js
+++ src-example/main.js
@@ -1,9 +1,10 @@
 'strict'
 
+
 function run () {
   const game = Minefield.getInstance(Minefield.DEFAULTS.MEDIUM)
 
-  new Bot({ game, delay: 2000 })
+  new Bot({ game, mode: Bot.MODE.FAST })
   new MinefieldController({ game })
 
   game.start()
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -2,51 +2,131 @@ import * as Minefield from '../main'
 
 class Bot {
   constructor (options = {
-    delay: Bot.MANUAL
+    mode: Bot.MANUAL
   }) {
     this.game = options.game
-    this.delay = options.delay
+    this.delay = options.mode
+    this.nextList = []
 
-    this.game.addListener(Minefield.EVENTS.ALL, (data) => this.autoRun(data), this.delay)
+    this.game.addListener(Minefield.EVENTS.ALL, () => this.autoRun(), this.delay)
   }
 
-  autoRun (data) {
+  autoRun () {
     if (this.delay > Bot.MODE.MANUAL) {
-      setTimeout(() => this.run(data), this.delay)
+      setTimeout(() => this.run(), this.delay)
     }
   }
 
-  run (data) {
-    switch (data.event) {
-      case Minefield.EVENTS.START:
-      case Minefield.EVENTS.NEXT_TURN:
-        this.calculate(data)
-        break
+  run () {
+    this.state = this.game.getState()
 
-      case Minefield.EVENTS.FINISH:
-        console.log(`Heeey, i'm the Minefield BOT and i'm ${data.state.status === Minefield.STATUS.LOSS } ? 'LOSS! =(' : 'WIN! \ o /' game`  )
-        break
+    if (this.state.status === Minefield.STATUS.PLAYING) {
+      this.calculate()
+      this.openNext()
+    } else {
+      console.log(`Heeey, i'm the Minefield BOT and i'm \
+        ${this.state.status === Minefield.STATUS.LOSS
+          ? 'LOSS! =('
+          : 'WIN! \ o /'
+        } game`
+      )
     }
   }
 
-  calculate(data) {
-    console.log('====> AGORA FUDEU!')
-    this.openRandom(data)
+  calculate () {
+    this.calcAvailable()
+
+    if (this.nextList.length || this.state.checked < 4) {
+      return
+    }
+
+    this.calcAround()
+    this.calcNextList()
   }
 
-  openRandom (data) {
-    const rand = (size) => Math.floor(Math.random() * size)
+  calcAvailable () {
+    this.available = [].concat(
+      ...this.state.board.map(
+        (cols, row) => cols.map(
+          (value, col) => (value === Minefield.CHECKERS.EMPTY
+            ? { row, col } : null)
+        )
+      )
+    ).filter(values => values !== null)
+  }
 
-    this.game.open(
-      rand(data.state.rows),
-      rand(data.state.cols)
-    )
+  calcAround() {
+    this.available.forEach(checker => {
+      checker.around = 0
+      checker.aroundList = []
+      checker.closedList = []
+
+      const { rows, cols } = this.state
+
+      if (checker.row === 0 && (checker.col === 0 || checker.col === cols - 1)
+      || checker.row === rows - 1 && (checker.col === 0 || checker.col === cols - 1)) {
+        checker.around += 5
+      } else if (checker.row === 0
+      || checker.row === rows - 1
+      || checker.col === 0
+      || checker.col === cols -1) {
+        checker.around += 3
+      }
+
+      for (let row = checker.row - 1; row <= checker.row + 1; row++) {
+        for (let col = checker.col - 1; col <= checker.col + 1; col++) {
+          if (this.state.board[row] !== undefined
+            && this.state.board[row][col] !== undefined) {
+              if (this.state.board[row][col] !== Minefield.CHECKERS.EMPTY) {
+                checker.around++
+                checker.aroundList.push(this.state.board[row][col])
+              } else {
+                checker.closedList.push(this.state.board[row][col])
+              }
+          }
+        }
+      }
+    });
+  }
+
+  calcNextList() {
+    this.nextList = []
+    this.available.forEach((checker) => {
+      if (checker.around === 8) {
+        checker.flag = true
+        this.nextList.push(checker)
+      }
+    })
+  }
+
+  open(checker) {
+    console.log('OPEN: ', checker)
+
+    if (checker.flag) {
+      this.game.flag(checker.row, checker.col)
+    } else {
+      this.game.open(checker.row, checker.col)
+    }
+  }
+
+  openNext() {
+    if (this.nextList.length) {
+      this.open(this.nextList.pop())
+    } else {
+      this.openRandom()
+      console.log('========================> RANDOM')
+    }
+  }
+
+  openRandom () {
+    this.open(this.available[Math.floor(Math.random() * this.available.length)])
   }
 }
 
 Bot.MODE = {
   FAST: 1000,
   MANUAL: -1,
+  NORMAL: 2000,
   SLOW: 4000
 }
 
